a5fb61fc445d25db93d8d540c0d72540d5fb5075
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2024-09-25 17:33:11 -0600

    print nuevo

--- biquad.cpp
+++ biquad.cpp
@@ -9,6 +9,10 @@ biquad::biquad(std::vector<std::vector<float>>& coefsIn) {
             std::cout << "Coeficiente cargado: " << this->coefs[i][j] << std::endl;
         }
     }
+    // Obtener el número de etapas del filtro
+    this->getMaxOrder();
+    std::cout << "Orden del filtro cargado: " << this->maxOrder << std::endl;
+
 }
 
 // Extraer los coeficientes del numerador de una etapa específica
@@ -23,8 +27,8 @@ std::vector<float> biquad::numExtractor(int order) {
 // Extraer los coeficientes del denominador de una etapa específica
 std::vector<float> biquad::denExtractor(int order) {
     std::vector<float> a(3);  // Inicializar el vector con tamaño 3
-    for (int i = 3; i < 6; i++) {
-        a[i] = this->coefs[order][i];  // Ajuste de índice
+    for (int i = 0; i < 3; i++) {
+        a[i] = this->coefs[order][i+3];  // Ajuste de índice
     }
     return a;
 }
@@ -34,19 +38,20 @@ void biquad::getMaxOrder() {
     this->maxOrder = this->coefs.size();  // Número de etapas es el tamaño del vector de coeficientes
 }
 
-// Aplicar el filtro a una señal de entrada utilizando los coeficientes de numerador y denominador
 std::vector<float> biquad::applyFilter(const std::vector<float>& input, const std::vector<float>& b, const std::vector<float>& a) {
     size_t N = input.size();
     size_t M = b.size();  // Tamaño de los coeficientes del numerador
     std::vector<float> output(N, 0);
 
-    for (size_t n = 0; n < 100; ++n) {
+
+    for (size_t n = 0; n < N; ++n) {
         // Convolución del numerador
         for (size_t i = 0; i < M; ++i) {
             if (n >= i) {
                 output[n] += b[i] * input[n - i];
             }
         }
+
         // Convolución del denominador, omitiendo a[0]
         for (size_t j = 1; j < M; ++j) {  // Empieza desde a[1]
             if (n >= j) {
@@ -58,6 +63,7 @@ std::vector<float> biquad::applyFilter(const std::vector<float>& input, const st
     return output;
 }
 
+
 // Procesar la señal de entrada en bloques
 void biquad::process(int nframes, const float *const in, std::vector<float>& out) {
     std::vector<float> temp(nframes);
@@ -67,13 +73,10 @@ void biquad::process(int nframes, const float *const in, std::vector<float>& out
         temp[i] = in[i];
     }
 
-    // Obtener el número de etapas del filtro
-    this->getMaxOrder();
-
     // Aplicar el filtro por cada etapa
-    //for (int i = 0; i < this->maxOrder; i++) {
-        temp = this->applyFilter(temp, this->numExtractor(0), this->denExtractor(0));
-    //}
+    for (int i = 0; i < this->maxOrder; i++) {
+        temp = this->applyFilter(temp, this->numExtractor(i), this->denExtractor(i));
+    }
 
     for (int i = 0; i < nframes; i++) {
         out[i] = temp[i];
