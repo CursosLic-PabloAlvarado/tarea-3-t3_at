86d7758814ccf3634f9850eb6a45ecc251d54fd8
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2024-09-28 21:28:28 -0600

    revive el cine 2

--- cascade.cpp
+++ cascade.cpp
@@ -15,7 +15,7 @@ cascade::cascade(std::vector<std::vector<float>> &coefsIn)
     }
 }
 
-/*  
+/*
 void cascade::process(int nframes, const float *const in, float * const out){
 
 
@@ -44,48 +44,61 @@ void cascade::process(int nframes, const float *const in, float * const out){
 
 */
 
+__m256 cascade::subProcessVector(int stage, __m256 inputVec)
+{
 
+    // Variables para almacenar el estado anterior de cada biquad
+    __m256 b0Vec = _mm256_set1_ps(this->stages[stage]->b0);
+    __m256 b1Vec = _mm256_set1_ps(this->stages[stage]->b1);
+    __m256 b2Vec = _mm256_set1_ps(this->stages[stage]->b2);
+    __m256 a1Vec = _mm256_set1_ps(this->stages[stage]->a1);
+    __m256 a2Vec = _mm256_set1_ps(this->stages[stage]->a2);
 
-void cascade::process(int nframes, const float * in, float * out)
+    // Estado del filtro (w1 y w2)
+    __m256 w1_pastVec = this->stages[stage]->w1_pastVec;
+    __m256 w2_pastVec = this->stages[stage]->w2_pastVec;
+
+    // Aplicar el filtro biquad transpuesto para 8 muestras
+    __m256 outputVec = _mm256_add_ps(_mm256_mul_ps(b0Vec, inputVec), w1_pastVec);
+    w1_pastVec = _mm256_add_ps(_mm256_sub_ps(_mm256_mul_ps(b1Vec, inputVec), _mm256_mul_ps(a1Vec, outputVec)), w2_pastVec);
+    w2_pastVec = _mm256_sub_ps(_mm256_mul_ps(b2Vec, inputVec), _mm256_mul_ps(a2Vec, outputVec));
+
+    // Guardar el estado actualizado
+    this->stages[stage]->w1_pastVec = w1_pastVec; // Usar la primera muestra de w1_pastVec
+    this->stages[stage]->w2_pastVec = w2_pastVec; // Usar la primera muestra de w2_pastVec
+
+    return outputVec;
+}
+
+void cascade::process(int nframes, const float *in, float *out)
 {
     const int simdWidth = 8;
 
     int i = 0;
-    
+
+    __m256 resultVec1 = inputVec;
+    __m256 resultVec2;
+    __m256 resultVec3;
     // Procesamiento con AVX para bloques de 8 muestras
     for (; i <= nframes - simdWidth; i += simdWidth)
     {
         // Cargar 8 muestras de entrada
-        __m128 inputVec = _mm_loadu_ps(&in[i]);
+        __m256 inputVec = _mm256_loadu_ps(&in[i]);
 
-        
-        // Procesar cada etapa biquad
-        for (int j = 0; j < this->maxOrder; j++)
+        if (this->maxOrder == 3)
         {
-            // Variables para almacenar el estado anterior de cada biquad
-            __m128  b0Vec = _mm_set1_ps(this->stages[j]->b0);
-            __m128  b1Vec = _mm_set1_ps(this->stages[j]->b1);
-            __m128  b2Vec = _mm_set1_ps(this->stages[j]->b2);
-            __m128  a1Vec = _mm_set1_ps(this->stages[j]->a1);
-            __m128  a2Vec = _mm_set1_ps(this->stages[j]->a2);
-
-            // Estado del filtro (w1 y w2)
-            __m128 w1_pastVec = this->stages[j]->w1_pastVec;
-            __m128 w2_pastVec = this->stages[j]->w2_pastVec;
-
-            // Aplicar el filtro biquad transpuesto para 8 muestras
-            __m128 outputVec = _mm_add_ps(_mm_mul_ps(b0Vec, inputVec), w1_pastVec);
-            w1_pastVec = _mm_add_ps(_mm_sub_ps(_mm_mul_ps(b1Vec, inputVec), _mm_mul_ps(a1Vec, outputVec)), w2_pastVec);
-            w2_pastVec = _mm_sub_ps(_mm_mul_ps(b2Vec, inputVec), _mm_mul_ps(a2Vec, outputVec));
-
-            // Guardar el estado actualizado
-            this->stages[j]->w1_pastVec = w1_pastVec; // Usar la primera muestra de w1_pastVec
-            this->stages[j]->w2_pastVec = w2_pastVec; // Usar la primera muestra de w2_pastVec
+            resultVec1 = this->subProcessVector(0, inputVec);
+            resultVec2 = this->subProcessVector(1, resultVec1);
+            resultVec3 = this->subProcessVector(1, resultVec2);
+        }
+        else
+        {
+            resultVec1 = this->subProcessVector(0, inputVec);
+            resultVec2 = this->subProcessVector(1, resultVec1);
+            resultVec3 = resultVec2
         }
 
         // Guardar el resultado procesado
-        _mm_storeu_ps(&out[i], outputVec);
+        _mm256256_storeu_ps(&out[i], resultVec3);
     }
 }
-
-
