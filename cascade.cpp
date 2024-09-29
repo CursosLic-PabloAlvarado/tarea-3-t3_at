e689d6f81ef9865d3448a8a40edced35c3b2f97b
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2024-09-28 18:49:41 -0600

    agarranse de las manos

--- cascade.cpp
+++ cascade.cpp
@@ -35,7 +35,7 @@ void cascade::process(int nframes, const float *const in, float * const out){
     //Procesar encadenando muestra por muestra
 
     std::vector<float> partialResults(this->maxOrder+1);
-    partialResults[0] = in[i];
+    partialResults[0] = in[0];
     for (int i=0; i<nframes ; i++){
         for (int j=0; j<this->maxOrder; j++){
             this->stages[i]->processOne(partialResults[j], partialResults[j+1]);
